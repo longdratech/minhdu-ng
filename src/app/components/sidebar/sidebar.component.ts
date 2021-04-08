4496215c4e5aa8db57fdc852469ef4eb8c2d6721
ngthanhthao201 <63150041+ngthanhthao201@users.noreply.github.com>
2021-04-08 15:14:24 +0700

    fix hardcode

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -7,15 +7,34 @@ import { Routes, RouterModule,ActivatedRoute } from '@angular/router';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-  isShow: boolean = false;
+  expanded: boolean = false;
+  iconHeader: string = "fa fa-th-large"
+  titleHeader: string = "PT1"
+  subTitle: any = [];
   constructor(
     private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
+    this.listSubMenu();
   }
-
-  showSubMenu(){
-    this.isShow = !this.isShow
+  showSubMenu(e: any){
+    if(e.submenu && e.submenu.length){
+      this.expanded = !this.expanded;
+    } 
+  }
+  listSubMenu(){
+    this.subTitle = [
+      {
+        path: '/report', title:'Báo cáo và thống kê', icon:'fa fa-th-large', submenu: []
+      },
+      {
+        path: '', title:'Nhân viên', icon:'fa fa-th-large', 
+        submenu: [
+         {path: '/danhsach', title:'Danh sách nhân viên', icon:''},
+         {path: '/salary', title:'Tính lương', icon:''},
+        ]
+      },     
+    ]
   }
 }
