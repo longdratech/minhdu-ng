de2a01ab7187f9680e989ab11d14270df9954d82
vansuphan <vansuphan88@gmail.com>
2021-04-10 20:21:39 +0700

    no messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TagNameComponent } from './tag-name/tag-name.component';
+import { TagNameComponent } from './components/tag-name/tag-name.component';
 
 @NgModule({
   declarations: [
