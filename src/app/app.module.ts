69864e5a2f9bff6ad13f7ea4f0fb84e93fee6921
vansuphan <37709216+vansuphan@users.noreply.github.com>
2021-04-11 09:51:26 +0700

    add tag name (#28)
    * feat: add tag name
    
    * style: format
    
    Co-authored-by: Long Trần (KMASoft) <tranlong.kma@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TagNameComponent } from './components/tag-name/tag-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TagNameComponent,
   ],
   imports: [
     BrowserModule,
