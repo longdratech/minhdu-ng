a026ffe0d832c6957ede88633025261da83042c9
ngthanhthao201 <63150041+ngthanhthao201@users.noreply.github.com>
2021-04-11 11:34:48 +0700

    sidebar (#29)
    * sidebar
    
    * remove even click
    
    Co-authored-by: vansuphan <vansuphan88@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,41 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgZorroAntdModule } from './ng-zorro-antd.module';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { SideBarComponent } from './components/sidebar/sidebar.component';
 import { TagNameComponent } from './components/tag-name/tag-name.component';
+
+registerLocaleData(en);
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    SideBarComponent,
     TagNameComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule
+
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+const listFetchMenu  = [
+  {
+    path: '/welcome', title: 'dashboard', icon: 'dashboard', subMenu: []
+  },
+  {
+    path: '/employee', title: 'Nhân viên', icon: 'user',
+    subMenu: [
+      { path: '/employee', title: 'Danh sách nhân viên', icon: '' },
+    ]
+  },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+
+export class SideBarComponent implements OnInit {
+
+  @Input() isCollapsed!: boolean | string;
+  @Input() listMenu = listFetchMenu;
+  @Input() indexActive: string | number = 0;
+  @Input() classActive: string | number = listFetchMenu[0].path;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
